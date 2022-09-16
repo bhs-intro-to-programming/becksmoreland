6f4311aab0ea881322f201ab8624989fa27d1c43
becksmoreland <107013426+becksmoreland@users.noreply.github.com>
2022-09-16 14:41:39 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -13,3 +13,5 @@ const averageOfTwo = (a, b) => (a + b) / 2;
 const averageOfThree = (a, b, c) => (a + b + c) / 3;
 
 const distance = (a, b) => Math.abs(a - b)
+
+const manhattanDistance = (x1, x2, y1, y2) => Math.abs(x1 - x2) + Math.abs(y1 - y2);
