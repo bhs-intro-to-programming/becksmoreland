5aed0c563255ffcc19d4ed0f948ec010e72eee91
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-09-21 15:14:58 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -41,6 +41,6 @@ const pigLatin = (myWord) => {
 }
 
 const advancedPigLatin = (myWord) => {
- return myWord.search(/[aeiou]/) === 0 ? myWord + 'way' : pigLatin;
+ return myWord.search(/[aeiou]/) === 0 ? myWord + 'way' : return pigLatin;
 }
  
