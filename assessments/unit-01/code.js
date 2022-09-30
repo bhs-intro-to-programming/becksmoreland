4919983f7c1d9afb66b11d033084e82b0f5c1109
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-09-30 15:16:08 -0700

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -9,3 +9,44 @@ const emit = (a, b, product) => {
 const averageWeight = (totalWeight, numOfItems) => {
   return totalWeight / numOfItems;
 }
+
+const hypotenuse = (legOne, legTwo) => {
+  return legOne ** 2 + legTwo ** 2;
+}
+
+const maxRadius = (width, height) => {
+  return (width + height) / 8;
+}
+
+const numCircles = (radius, width) => {
+  let d = radius * 2;
+  return width / d;
+}
+
+const offset = (widthPixels, figureWidth) => {
+  return (widthPixels - figureWidth)  ;
+}
+
+const canSleepIn = (isWeekday, onVacation) => {
+  return !isWeekday || onVacation;
+}
+
+const canGoToProm = (isSenior, invited, onExclusionList) => {
+  return isSenior || invited && !onExclusionList;
+}
+
+const getsSpeedingTicket = (mphDriving, isGrouchy) => {
+  return mphDriving > 65 && isGrouchy || mphDriving > 70 && !isGrouchy;
+}
+
+const moreThanTwiceAsLong = (string1, string2) => {
+    return string1.length > string2.length * 2;
+}
+const aFartherThanB = (number1, number2, number3) => {
+  return Math.abs(number1 - number3) > Math.abs(number2 - number3);
+}
+
+const firstHalf = (string) => {
+  return string.substring(0, Math.floor(string.length / 2));
+}
+
