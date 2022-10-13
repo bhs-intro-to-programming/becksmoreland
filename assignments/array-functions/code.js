625732b4e64ef4b298731e19ba9d445b3f941e38
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-10-13 15:25:52 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -9,7 +9,7 @@ const countTens = (array) => {
 }
 
 const sum = (array) => {
-  let totalSum === 0;
+  let totalSum = 0;
   for (let x = 0; x <= array.length; x++) {
     let totalSum = totalSum + array[x];
   } 
