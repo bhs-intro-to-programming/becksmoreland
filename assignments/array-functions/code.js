6968cff17ed258fe62a29a7df9a82ec658a01887
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-10-13 15:18:30 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,6 +1,6 @@
 const countTens = (array) => {
   let tensCount = 0;
-  for (x = 0; x <= array.length; x++) {
+  for (let x = 0; x <= array.length; x++) {
     if (array[x] === 10) {
       tensCount++;
     }
