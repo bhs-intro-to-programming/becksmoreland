a4e7d5584aa1b1d42b3a43535e37f5fa8085e676
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-10-17 15:05:44 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -38,7 +38,7 @@ const anyOverOneHundred = (array) => {
 const pyramid = (integer) => {
   let newArray = []
   for (let x = 0; x < integer; x++) {
-    newArray.push(integer - (integer - x)) * 2
+    newArray.push(integer - (integer - x + 1)) * 2
   }
   return newArray;
 }
