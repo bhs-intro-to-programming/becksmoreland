52ad0bc018ae0343cb53a2f36390fc7e8d38e434
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-10-19 15:03:26 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -5,13 +5,12 @@
 // is called an anonymous function. We'll discuss this in more detail in a few
 // weeks but for now you can just adapt this code.
 
-let currentBoard = {
-  [
+let currentBoard = [
     [0]
     [0]
     [0]
   ]
-}
+
 
 registerOnclick((x, y) => {
   drawText('X', x, y, 'black', Math.min(width, height) * 0.3);
