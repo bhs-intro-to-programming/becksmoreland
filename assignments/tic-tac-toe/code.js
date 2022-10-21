cf5538debc1783e7917fffa0a685bd740b67941f
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-10-21 15:16:58 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -8,7 +8,7 @@
 
 const ticTacToe = () => {
   for (let i = 0; i < 9; i++) {
-    if ((i + 2) % 2 = 0) {
+    if (i + 2 % 2 === 0) {
       registerOnclick((x, y) => {
         drawText('O', x, y, 'blue', Math.min(width, height) * 0.3);
       }
