1e3b0c92dd6f81864a6341f65ac59d944b579cab
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-10-21 15:16:18 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -7,16 +7,12 @@
 
 
 const ticTacToe = () => {
-  for(let i = 0; i < 9; i++) {
-    if ((i + 2) % === 0) {
+  for (let i = 0; i < 9; i++) {
+    if ((i + 2) % 2 = 0) {
       registerOnclick((x, y) => {
-        drawText('X', x, y, 'black', Math.min(width, height) * 0.3);
-       });
-    } else {
-    registerOnclick((x, y) => {
-      drawText('O', x, y, 'blue', Math.min(width, height) * 0.3);
-    });
+        drawText('O', x, y, 'blue', Math.min(width, height) * 0.3);
+      }
+    }
   }
 }
-
 
