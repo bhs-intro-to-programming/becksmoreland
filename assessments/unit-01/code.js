468c6798e5ed242312b70fd760d071cc41694c0a
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-10-26 15:06:20 -0700

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -99,7 +99,7 @@ const timesTable  = (number) => {
 const sumSquares = (number) => {
   const total = 0;
   for (let x = 0; x < number; x++) {
-    total = total + ((number - x)) ** 2);
+    total = total + ((number - x) ** 2);
   }
   return total;
 }
