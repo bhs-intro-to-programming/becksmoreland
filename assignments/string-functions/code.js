d51f325f6417fc176fb138889d64973c307cbb9b
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-10-27 14:45:31 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -1,3 +1,8 @@
 const findFnord = (string) => {
   return string.indexOf("fnord");
 }
+
+const stringContains = (string1, string2) => {
+  let containsSame = '';
+  return containsSame === string1.indexOf(string2);
+}
