b4903048694359deebe88a0f41f9c43bbc0bfd07
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-10-27 15:13:38 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -75,5 +75,5 @@ const lastThree = (string) => {
 }
 
 const capitalize = (string) => {
-  return string[0].toUpperCase() + string.substring(1);
+  return string[0].toUpperCase() + string.substring(1).toLowerCase();
 }
