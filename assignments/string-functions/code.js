ca759f3a8ef3805b9f3f519613df4f00e75c3a53
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-10-27 14:52:04 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -6,3 +6,7 @@ const stringContains = (string1, string2) => {
   let containsSame = '';
   return string2 === string1.substring(string1.indexOf(string2));
 }
+
+const firstAndLast = (string) => {
+  return string[0] + string[string.length - 1];
+}
