6c28f1d64de62aa470ba2dcbf3428f585dc188ee
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-10-31 15:19:34 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -40,7 +40,7 @@ const chessBoard = [
   [], [], [], [], [], [], [], [],
   [], [], [], [], [], [], [], [],
   [], [], [], [], [], [], [], [],
-  [wRook(200)], [wKnight(150)], [wBishop(100)], [wQueen(50)], [wKing()], [wBishop(- 50)], [wKnight( - 100)], [wRook()],
+  [wRook(200)], [wKnight(150)], [wBishop(100)], [wQueen(50)], [wKing()], [wBishop(- 50)], [wKnight( - 100)], [wRook( - 150)],
 ]
 
 chessBoard()
