0614ddbafb18b1f54e701ccd4fe71157aca963c9
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-10-31 15:18:03 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -12,24 +12,24 @@ const BLACK_KNIGHT = '♞';
 const BLACK_PAWN   = '♟';
 
 // Example of drawing one of the pieces
-const wKing = () => {
+const wKing = (space) => {
   drawText(WHITE_KING, width/2, height/2, 'black', 64);
 }
 
-const wQueen = () => {
+const wQueen = (space) => {
    drawText(WHITE_QUEEN, width/2 - 50, height/2, 'black', 64);
 }
 
-const wRook = () => {
+const wRook = (space) => {
    drawText(WHITE_ROOK, width/2 - 200, height/2 , 'black', 64);
 }
 
-const wKnight = () => {
+const wKnight = (space) => {
   drawText(WHITE_KNIGHT, width/2 - 150, height/2, 'black', 64);
 }
 
-const wBishop = () => {
-  drawText(WHITE_BISHOP, width/2 - 100, height/2, 'black', 64);
+const wBishop = (space) => {
+  drawText(WHITE_BISHOP, width/2 - space, height/2, 'black', 64);
 }
 
 const chessBoard = [
@@ -40,7 +40,7 @@ const chessBoard = [
   [], [], [], [], [], [], [], [],
   [], [], [], [], [], [], [], [],
   [], [], [], [], [], [], [], [],
-  [wRook()], [wKnight()], [wBishop()], [wQueen()], [wKing()], [wBishop()], [wKnight()], [wRook()],
+  [wRook()], [wKnight()], [wBishop(100)], [wQueen()], [wKing()], [wBishop()], [wKnight()], [wRook()],
 ]
 
 chessBoard()
