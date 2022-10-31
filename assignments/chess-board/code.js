1858d2bf359d00c87325cd921c2d30581fd4da66
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-10-31 15:25:52 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -44,8 +44,12 @@ const bBishop = (space) => {
   drawText(BLACK_BISHOP, width/2 - space, height/2 - 75, 'black', 64)
 }
 
+const bKnight = (space) => {
+    drawText(BLACK_KNIGHT, width/2 - space, height/2 - 75, 'black', 64)
+}
+
 const chessBoard = [
-  [bKing()], [bQueen(50)], [bBishop(100)], [], [], [], [], [],
+  [bRook(200)], [bKnight(150)], [bBishop(100)], [bQueen(50)], [bKing()], [bBishop(- 50)], [bKnight(- 100)], [bRook(-150)],
   [], [], [], [], [], [], [], [],
   [], [], [], [], [], [], [], [],
   [], [], [], [], [], [], [], [],
