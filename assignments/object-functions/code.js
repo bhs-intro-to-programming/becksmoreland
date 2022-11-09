f2f796df58824c33112187d429b610f20d9fcbfc
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-11-09 14:41:46 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -30,7 +30,7 @@ const sumSalaries = (array) => {
 
 const newHighScore = (currentScore, array) => {
   let newScore = currentScore;
-  for (let x = 0; x < array; x++) {
+  for (let x = 0; x < array.length; x++) {
     if (currentScore < array[x]["score"]) {
       newScore = array[x]["score"];
     }
