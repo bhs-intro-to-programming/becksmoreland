f1945ad2ce49f010205d8efa47af7b5c35441989
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-11-10 14:48:14 -0800

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -7,7 +7,7 @@ const findFnord = (string) => {
 }
 
 const stringContains = (string1, string2) => {
-  string2 === string1.substring(string1.indexOf(string2), string1.indexOf(string2) + string2.length)
+  return string2 === string1.substring(string1.indexOf(string2), string1.indexOf(string2) + string2.length)
 }
 
 const firstAndLast = (string) => {
