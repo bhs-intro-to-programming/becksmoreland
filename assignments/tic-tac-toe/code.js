61337f2d2c52f802bd143eec12d39dbbac011bb2
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-11-15 15:04:17 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -5,16 +5,17 @@
 // is called an anonymous function. We'll discuss this in more detail in a few
 // weeks but for now you can just adapt this code.
 
-let playerOne = {current: 'X'};
+let playerOne = 0;
+
+let currentLetter = 'X';
 
 const keepTrack = () => {
   for (let x = 0; x < 9; x++) {
-    if ((x + 2) % 2 === 0) {
-      playerOne.current === 'O';
+    if ((playerOne + 2) % 2 === 0) {
+      currentLetter = 'O'
     } else {
       playerOne.current === 'X'
     }
-    return x;
 }
 }
 
@@ -23,8 +24,8 @@ const keepTrack = () => {
 
 
 registerOnclick((x, y) => {
-  drawText(playerOne.current, x, y, 'blue', Math.min(width, height) * 0.3);
-  keepTrack()
+  drawText(currentLetter, x, y, 'blue', Math.min(width, height) * 0.3);
+  playerOne++
 });
 
 
@@ -37,3 +38,5 @@ drawLine(150, height / 3 + 50, 400, height / 3 + 50, 'black');
 drawLine(width / 3, 0, width / 3, 200, 'black');
 
 drawLine(width / 3 + 100, 0, width / 3 + 100, 200, 'black');
+
+keepTrack()
