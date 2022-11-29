e3b8839cd8f3d868cc36d207d5ac672f83b82bdc
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-11-29 14:48:02 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -34,12 +34,18 @@ registerOnclick((x, y) => {
   
   playerOne++
 
-  if ((x < width / 3) && (y < height / 3)) {
+  if ((x < width / 3) && (y > height / 3)) {
     currentPos[1][1] = 'X';
   } else if (x > width / 3 && x < width / 3 + 100 && y > height / 3 - 20 ) {
     currentPos[1][2] = 'O';
   } else if (x > width / 3 + 100 && y > height / 3) {
-    currentPos[1][3] = 'X'
+    currentPos[1][3] = 'X';
+  } else if (x < width / 3 && y < height / 3 && y > height / 3 + 20) {
+    currentPos[2][1] = 'O';
+  } else if (x > width / 3 && x < width / 3 + 100 && y < height / 3 && y > height / 3 + 20) {
+    currentPos[2][2] = 'X';
+  } else if (x > width / 3 + 100 && y < height / 3 && y > height / 3 + 20) {
+    currentPos[2][3] = 'O';
   }
 });
 
