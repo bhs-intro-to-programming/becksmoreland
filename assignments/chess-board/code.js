f7e99c9ddfee1bb4d0eddffe330f15ce7b381051
Beckham <107013426+becksmoreland@users.noreply.github.com>
2022-12-02 15:11:20 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -60,7 +60,7 @@ const bRook = (space) => {
 }
 
 const chessBoard = [
-  [bRook(146), bKnight(110), bBishop(75), bQueen(50), bKing(), bBishop(- 50), bKnight(- 100), bRook(-150)],
+  [bRook(146), bKnight(110), bBishop(70), bQueen(50), bKing(), bBishop(- 50), bKnight(- 100), bRook(-150)],
   [],
   [],
   [],
